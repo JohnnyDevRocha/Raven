3473de9a740210d23df3e20efb1e4c199160c037
JohnnyDev2001 <johnny.devcode@gmail.com>
2022-10-24 23:59:59 -0300

    premake config

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+workspace "Raven"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Raven"
+	location "Raven"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include;"
+	}
+RV_PLATFORM_WINDOWS;RV_BUILD_DLL;
