f6a4934e54d2ce87197153c06b6cea5b78beb369
JohnnyDev2001 <johnny.devcode@gmail.com>
2022-10-25 22:15:56 -0300

    Premake5 config

--- premake5.lua
+++ premake5.lua
@@ -28,4 +28,78 @@ project "Raven"
 	{
 		"%{prj.name}/vendor/spdlog/include;"
 	}
-RV_PLATFORM_WINDOWS;RV_BUILD_DLL;
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		sytemversion "10.0"
+
+		defines
+		{
+			"RV_PLATFORM_WINDOWS",
+			"RV_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			{"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"}
+		}
+
+	filter "configurations::Debug"
+		defines "RV_DEBUG"
+		symbols "On"
+
+	filter "configurations::Release"
+		defines "RV_RELEASE"
+		optimize "On"
+
+	filter "configurations::Dist"
+		defines "RV_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include;"
+		"Raven/src"
+	}
+
+	links
+	{
+		"Raven"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		sytemversion "10.0"
+
+		defines
+		{
+			"RV_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations::Debug"
+		defines "RV_DEBUG"
+		symbols "On"
+
+	filter "configurations::Release"
+		defines "RV_RELEASE"
+		optimize "On"
+
+	filter "configurations::Dist"
+		defines "RV_DIST"
+		optimize "On"
